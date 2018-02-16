8545c6ea5e06c28d8c6d1443c63cc80415f7e6ad
kintela <roberto.quintela@gmail.com>
2018-02-16 12:22:21 +0100

    Patron para interfrir en savechanges()

--- NinjaDomain.DataModels/NinjaContext.cs
+++ NinjaDomain.DataModels/NinjaContext.cs
@@ -1,5 +1,8 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
 using Ninja.Domain.Clases;
+using Ninja.Domain.Clases.interfaces;
 
 
 namespace NinjaDomain.DataModels
@@ -9,6 +12,41 @@ namespace NinjaDomain.DataModels
         public DbSet<Ninja.Domain.Clases.Ninja> Ninjas { get; set; }
         public DbSet<Ninja.Domain.Clases.Clan> Clan { get; set; }
         public DbSet<Ninja.Domain.Clases.NinjaEquipment> Equipment { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Types().
+                Configure(c=>c.Ignore("IsDirty"));
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var history in this.ChangeTracker.Entries()
+                .Where(e=>e.Entity is IModificacionHistory && (e.State==EntityState.Added ||
+                        e.State==EntityState.Modified))
+                .Select(e=>e.Entity as IModificacionHistory)
+            )
+            {
+                history.DateModified = DateTime.Now;
+                if (history.DateCreated == DateTime.MinValue)
+                {
+                    history.DateCreated = DateTime.Now;
+                }
+            }
+
+            int result = base.SaveChanges();
+
+            foreach (var history in this.ChangeTracker.Entries()
+                .Where(e=>e.Entity is IModificacionHistory)
+                .Select(e=>e.Entity as IModificacionHistory)
+                )
+            {
+                history.IsDirty = false;   
+            }
+
+            return result;
+        }
     }
 }
  
