1ff96afa8375fd1b52d6ba466e5866efb75a74ec
kintela <roberto.quintela@gmail.com>
2018-02-19 11:34:20 +0100

    Conneccted repository

---
+++ NinjaDomain.DataModels/ConnectedRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninja.Domain.Clases;
+
+namespace NinjaDomain.DataModels
+{
+    public class ConnectedRepository
+    {
+        private readonly NinjaContext _context = new NinjaContext();
+
+        //public Ninja.Domain.Clases.Ninja GetNinjaWithEquipment(int id)
+        //{
+        //    return _context.Ninjas.Include(n =>n.EquipmentOwned).FirstOrDefault(n => n.Id == id);
+        //}
+
+        public Ninja.Domain.Clases.Ninja GetNinjaById(int id)
+        {
+            return _context.Ninjas.Find(id);
+        }
+
+        public List<Ninja.Domain.Clases.Ninja> GetNinjas()
+        {
+            return _context.Ninjas.ToList();
+        }
+
+        public IEnumerable GetClanList()
+        {
+            return _context.Clans.OrderBy(c => c.ClanName).Select(c => new { c.Id, c.ClanName }).ToList();
+        }
+
+        public ObservableCollection<Ninja.Domain.Clases.Ninja> NinjasInMemory()
+        {
+            if (_context.Ninjas.Local.Count==0)
+            {
+                GetNinjas();
+            }
+
+            return _context.Ninjas.Local;
+        }
+
+        public void Save()
+        {
+            RemoveEmptyNewNinjas();
+            _context.SaveChanges();
+        }
+
+        public Ninja.Domain.Clases.Ninja NewNinja()
+        {
+            var ninja = new Ninja.Domain.Clases.Ninja();
+            _context.Ninjas.Add(ninja);
+            return ninja;
+        }
+
+        private void RemoveEmptyNewNinjas()
+        {
+            //No puedes eleminar o añadir a una coleccion en un bule foreach
+            for (var i = _context.Ninjas.Local.Count; i >0; i--)
+            {
+                var ninja = _context.Ninjas.Local[i - 1];
+                if (_context.Entry(ninja).State==EntityState.Added && !ninja.IsDirty)
+                {
+                    _context.Ninjas.Remove(ninja);
+                }
+            }
+        }
+
+        public void DeleteCurrentNinja(Ninja.Domain.Clases.Ninja ninja)
+        {
+            _context.Ninjas.Remove(ninja);
+            Save();
+        }
+
+        public void DeleteEquipment(ICollection equipmentList)
+        {
+            foreach (NinjaEquipment equip in equipmentList)
+            {
+                _context.Equipment.Remove(equip);
+            }
+        }
+    }
+}
